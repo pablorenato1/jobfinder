61746b6ed5283a391aaeadc26ea37b594199cbf0
Pablo Renato <56098864+pablorenato1@users.noreply.github.com>
2022-09-02 21:35:30 -0300

    first insertion in the db

--- routes/jobs.js
+++ routes/jobs.js
@@ -11,8 +11,9 @@ router.get('/test', (req, ans) => {
 
 // add job by post
 router.post('/add', (req, ans)=> {
-
+    // console.log("1")
     let {title,salary,company,description,email,new_job} = req.body;
+    // console.log("2")
 
     // insert 
     Job.create({
@@ -23,7 +24,10 @@ router.post('/add', (req, ans)=> {
         new_job,
         email
     })
-    .then(() => ans.redirect('/'))
+    .then(() => {
+        // ans.redirect('/')
+        ans.send("sql added")
+    })
     .catch(err => console.log(err));
 
 })
